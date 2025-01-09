8f346fbc1373a23f9096c32b174784d5a0a2f4bb
MrRaja1 <mail2myofficial@gmail.com>
2025-01-10 00:42:04 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -16,16 +16,47 @@ void main() {
     await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('RIDES'), findsOneWidget);
+    expect(find.text('STORIES'), findsOneWidget);
+    expect(find.text('MARKET'), findsOneWidget);
+    expect(find.text('GARAGE'), findsOneWidget);
+    expect(find.text('Invite'), findsOneWidget);
+    expect(find.text('Register Now'), findsOneWidget);
+    expect(find.text('..... - .....INR'), findsOneWidget);
+    
+    expect(find.text('abc'), findsNothing);
+    expect(find.byIcon(Icons.notifications), findsOne);
+    expect(find.byIcon(Icons.image_outlined), findsOne);
+    expect(find.byIcon(Icons.credit_card), findsOne);
+    expect(find.byIcon(Icons.menu), findsOne);
+    expect(find.byIcon(Icons.add_call), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    //await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.byIcon(Icons.notifications));
+    await tester.tap(find.byIcon(Icons.image_outlined));
+    await tester.tap(find.byIcon(Icons.credit_card));
+    await tester.tap(find.byIcon(Icons.menu));
     await tester.pump();
 
     // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // expect(find.text('0'), findsNothing);
+    // expect(find.text('1'), findsOneWidget);
+
+    expect(find.text('RIDES'), findsOneWidget);
+    expect(find.text('STORIES'), findsOneWidget);
+    expect(find.text('MARKET'), findsOneWidget);
+    expect(find.text('GARAGE'), findsOneWidget);
+    expect(find.text('Invite'), findsOneWidget);
+    expect(find.text('Register Now'), findsOneWidget);
+    expect(find.text('..... - .....INR'), findsOneWidget);
+
+    expect(find.text('abc'), findsNothing);
+    expect(find.byIcon(Icons.notifications), findsOne);
+    expect(find.byIcon(Icons.image_outlined), findsOne);
+    expect(find.byIcon(Icons.credit_card), findsOne);
+    expect(find.byIcon(Icons.menu), findsOne);
+    expect(find.byIcon(Icons.add_call), findsNothing);
   }
              );
 }
